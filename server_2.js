6c145bc92e13e5497e0846da02493f5fb4e81179
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2020-02-24 16:23:34 +0900

    fin

--- server_2.js
+++ server_2.js
@@ -4,18 +4,16 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+        // postデータ
+        var postData = "";
 
         // url取得
         var pathname = url.parse(request.url).pathname;
+
         console.log("Request for " + pathname + " received");
 
         // ルータ
-        route(handle, pathname, response);
-
-        // ブロッキングへの対応
-        // response.writeHead(200, { "Content-Type": "text/plain" });
-        // response.write("Hello World");
-        // response.end();
+        route(handle, pathname, response, request);
     }
 
     http.createServer(onRequest).listen(8000);
